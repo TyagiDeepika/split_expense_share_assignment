cee95ca7bd383e4b7a5ddc48067355552a32bb13
Deepika Tyagi <100507594+TyagiDeepika@users.noreply.github.com>
2024-05-23 12:18:51 +0530

    Update admin.py

--- expense_share/core/admin.py
+++ expense_share/core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Expense, Balance
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('mobile_number',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('mobile_number',)}),
+    )
+    list_display = ('username', 'email', 'mobile_number', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'mobile_number')
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('payer', 'amount', 'description', 'split_type', 'date')
+    search_fields = ('payer__username', 'description')
+    list_filter = ('split_type', 'date')
+
+
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'amount')
+    search_fields = ('from_user__username', 'to_user__username')
+    list_filter = ('from_user', 'to_user')
+
+
+
+
+# Register the models with the custom admin classes
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Balance, BalanceAdmin)
